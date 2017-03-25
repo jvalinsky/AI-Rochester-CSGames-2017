8efca5e29b4bbdf1dfef1cfc73be0c020a6b1f5c
J. Hassler Thurston <jthurst3@u.rochester.edu>
2017-03-25 10:16:08 -0400

    update stupid client

--- src/stupid_client.py
+++ src/stupid_client.py
@@ -15,8 +15,8 @@ class StupidClient(HockeyClient):
     def __init__(self, name, debug):
         self.name = name
         self.debug = debug
-        self.X = 5
-        self.Y = 5
+        self.X = 7
+        self.Y = 7
 
     def lineReceived(self, line):
         line = line.decode('UTF-8')
@@ -30,6 +30,8 @@ class StupidClient(HockeyClient):
             nums = re.findall(r'\d+', line)
             self.X = int(nums[0])
             self.Y = int(nums[1])
+        elif 'polarity of the goal has been inverted' in line:
+            self.goal = 15 if self.goal == -1 else -1
         elif 'did go' in line:
             d1 = line.split(' ')[-4]
             d2 = line.split(' ')[-3]
@@ -47,7 +49,7 @@ class StupidClient(HockeyClient):
             if 'north' in line:
                 self.goal = -1
             else:
-                self.goal = 11
+                self.goal = 15
 
     def move_random(self):
          return Action.from_number(random.randint(0, 7))
@@ -58,32 +60,32 @@ class StupidClient(HockeyClient):
 
     def move(self):
         if self.goal == -1:
-            if self.X < 5 and self.Y > 0:
+            if self.X < 7 and self.Y > 0:
                 return "north east"
-            if self.Y == 0 and self.X < 5:
+            if self.Y == 0 and self.X < 7:
                 return "south east"
-            if self.Y == 0 and self.X > 5:
+            if self.Y == 0 and self.X > 7:
                 return "south west"
-            if self.Y > 0 and self.X > 5:
+            if self.Y > 0 and self.X > 7:
                 return "north west"
-            if self.X == 5:
+            if self.X == 7:
                 return "north"
         else:
-            if self.X < 5 and self.Y < 10:
+            if self.X < 7 and self.Y < 14:
                 return "south east"
-            if self.Y == 10 and self.X < 5:
+            if self.Y == 14 and self.X < 7:
                 return "north east"
-            if self.Y == 10 and self.X > 5:
+            if self.Y == 14 and self.X > 7:
                 return "north west"
-            if self.Y < 10 and self.X > 5:
+            if self.Y < 14 and self.X > 7:
                 return "south west"
-            if self.X == 5:
+            if self.X == 7:
                 return "south"
 
 
 class StupidClientFactory(ClientFactory):
     def __init__(self, debug):
-        self.name =  "Canadian Immigrants"
+        self.name =  "Canadian Immigrants2"
         self.debug = debug
 
     def buildProtocol(self, addr):
