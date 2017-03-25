f5ea2a6767211e8d169229b0b1d8c10b419e5d86
Jack Valinsky <jvalinsk@u.rochester.edu>
2017-03-24 21:49:50 -0400

    updated stupid client

--- src/stupid_client.py
+++ src/stupid_client.py
@@ -9,7 +9,30 @@ from hockey.action import Action
 from client import HockeyClient, ClientFactory
 
 class StupidClient(HockeyClient):
+
+    def lineReceived(self, line):
+        line = line.decode('UTF-8')
+        if self.debug:
+            print('Server said:', line)
+        if '{} is active player'.format(self.name) in line:
+            if not ( 'invalid move' in line):
+                self.invalid = False
+                self.play_game()
+            else:
+                self.invalid = True
+                self.play_game()
+        elif 'ball is at' in line:
+            nums = re.findall(r'\d+', line)
+            self.X = int(nums[0])
+            self.Y = int(nums[1])
+        elif 'your goal is' in line:
+            if 'north' in line:
+                self.goal = -1
+            else:
+                self.goal = 11
     def move(self):
+        if self.invalid:
+            return Action.from_number(random.randint(0, 7))
         if self.goal == -1:
             if self.X < 0 and self.Y > 0:
                 return "north east"
